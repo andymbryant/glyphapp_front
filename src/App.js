9756e6e08e89d980ea2afbf817fb53bed865948c
Andy Bryant <a.mcdermid.bryant@gmail.com>
2018-03-26 19:38:09 -0700

    V1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Reader from './Reader.js';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Reader/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Reader.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import CSVReader from "react-csv-reader";
+import axios from 'axios';
+import "./App.css";
+
+const sendData = data => {
+    axios.post('http://127.0.0.1:3001/save', {
+        data: data
+        })
+        .then(function (response) {
+            alert(response.body);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    alert("Your data has been uploaded successfully.")
+};
+
+class Reader extends Component {
+    render() {
+        return (
+            <div className="container">
+                <CSVReader
+                    cssClass="react-csv-input"
+                    label="Upload Data Set"
+                    onFileLoaded={sendData}
+                />
+            </div>
+            );
+        }
+    }
+
+export default Reader;
